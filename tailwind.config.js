d52f476219cb7a47737d2024e2ace351a45058a4
Hieu Phan Xuan <xuanhieu@192.168.1.10>
2024-03-27 02:44:54 +0700

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,8 @@ module.exports = {
             },
             screens: {
                 'light': {'raw': '(prefers-color-scheme: light)'},
-                'dark': {'raw': '(prefers-color-scheme: light)'}
+                'dark': {'raw': '(prefers-color-scheme: light)'},
+                'dark-mode': null,
             }
         }
     },
