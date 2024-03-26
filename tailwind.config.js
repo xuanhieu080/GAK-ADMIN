21dc9836a7f84ead891133febb0ae99040312c05
Hieu Phan Xuan <xuanhieu@192.168.1.10>
2024-03-27 02:53:48 +0700

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
+    darkMode: false,
     content: ["./resources/js/**/*.html", "./resources/js/**/*.vue"],
     theme: {
         extend: {
