1cce074dc50214439a9a2f569cce2b79855873bd
Hieu Phan Xuan <xuanhieu@192.168.1.11>
2024-04-21 18:14:19 +0700

    Update

--- app/Services/Product/ProductService.php
+++ app/Services/Product/ProductService.php
@@ -390,6 +390,7 @@ class ProductService
             sort($attributes);
             return [
                 'name'             => "$product->name$name",
+                'code'             => Support::genCode('ProductVariant', 'code', 16),
                 'product_id'       => $product->id,
                 'option_name'      => trim($name, ', '),
                 'options'          => json_encode($attributes),
