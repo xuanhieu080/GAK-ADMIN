c5f52c01f85f0cf05fce26849f025f206cbf007a
Hieu Phan Xuan <xuanhieu@192.168.1.10>
2024-04-24 13:52:53 +0700

    Update

--- app/Http/Controllers/SpaController.php
+++ app/Http/Controllers/SpaController.php
@@ -13,6 +13,7 @@ class SpaController extends BaseController
      */
     public function __invoke()
     {
+    \Illuminate\Support\Facades\Config::set('app.locale', 'vi');
         return view('index', ['config' => Config::pluck('description', 'code')]);
     }
 }
