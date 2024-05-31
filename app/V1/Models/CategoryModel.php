3aaf5276c20e9291cff8da7805f5c97f64434d86
HieuPX <xuanhieu.ht96@gmail.com>
2024-05-31 17:28:38 +0700

    Update

--- app/V1/Models/CategoryModel.php
+++ app/V1/Models/CategoryModel.php
@@ -178,7 +178,7 @@ class CategoryModel extends AbstractModel
             },
         ])->where('is_active', 1)
             ->where('show_header', 1)
-            ->orderBy('name')
+            ->orderBy('order')
             ->get();
 
 // Đối với mỗi category, sẽ load thêm các products từ descendants
